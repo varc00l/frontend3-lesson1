d3c84d4e8950dc9390fbdf1fa7c0db376cbf2e87
varc00l <ivalchuk4@gmail.com>
2024-04-02 20:39:41 +0300

    add

---
+++ script.js
@@ -0,0 +1,31 @@
+const age = '14'
+console.log(age)
+
+const name = 'Ivan'
+console.log(name)
+
+const isStudent = 'false'
+console.log(isStudent)
+
+const myString = 'Життя це те, що з тобою відбувається, поки ти будуєш плани'
+console.log(myString)
+
+const myNumber = 25 + 10
+console.log(myNumber)
+
+const myNull = 'null'
+console.log(myNull)
+
+const yourname = prompt('Your name')
+wwwealert(`Hello ${yourname}`)
+
+const win = confirm("Ви підтверджуєте")
+alert(win)
+
+const r = confirm("Дія небезпечна для коричстувача");
+if (r == true) {
+  x = "Дякую за підтвердження!";
+} else {
+  x = "Дію відмінено!";
+}
+alert (x);
